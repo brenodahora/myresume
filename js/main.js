d33b2a64409f9dfd9fddcbce08bff9bb99a71c3d
Breno Aires da Hora <86855910+brenodahora@users.noreply.github.com>
2023-10-29 18:08:04 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 console.log('Hello World!');
+
+fetch('test.txt')
+  .then(response => response.text())
+  .then(text => {
+    const array = text.split("\n");
+    console.log(array);
+  })
+
+console.log('Test!');
+
+fetch('js/test.txt')
+  .then(response => response.text())
+  .then(text => {
+    const array = text.split("\n");
+    console.log(array);
+  })
